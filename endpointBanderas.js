5352666d0a53a9d8a59c23b98a225bf213578849
dannycornelio <licdannycornelio@gmail.com>
2023-07-18 09:44:28 -0600

    cambio de nombre a datos generales

---
+++ endpointBanderas.js
@@ -0,0 +1,39 @@
+let buscarBtn = document.getElementById("buscarBtn");
+let paisInp = document.getElementById("paisInp");
+
+paisInp.addEventListener("paisInp", () => {
+    let texto = paisInp.value;
+    let textoMinuscula = texto.toLowerCase();
+  
+    if (texto !== textoMinuscula) {
+      input.value = textoMinuscula;
+    }
+  });
+
+buscarBtn.addEventListener("click", () => {
+    let paisName = paisInp.value.toLowerCase(); // Convertir a minúsculas
+    let finalURL = `https://restcountries.com/v3.1/name/${paisName}?fullText=true`;
+    console.log(finalURL);
+    fetch(finalURL)
+        .then((response) => response.json())
+        .then((data) => {
+          
+            resultado.innerHTML = `
+            <img src="${data[0].flags.svg}" class="flagImg">
+            <h2> ${data[0].name.common} </h2>
+            <div class="wrapper">
+                <div class="dataWrapper">
+                     <h4>Capital</h4>
+                     <span>${data[0].capital[0]}</span>
+                </div>
+            </div>
+            <div class="wrapper">
+                <div class="dataWrapper">
+                     <h4>Continente</h4>
+                     <span>${data[0].continents[0]}</span>
+                </div>
+            </div>
+            `
+            
+        });  
+});
