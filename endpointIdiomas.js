864fbe0cbd21fbd1f68a862f068eab40bbb0ab03
dannycornelio <licdannycornelio@gmail.com>
2023-07-18 10:20:20 -0600

    modificacion en los demas endpoint

---
+++ endpointIdiomas.js
@@ -0,0 +1,34 @@
+let buscarBtn = document.getElementById("buscarBtn");
+let paisInp = document.getElementById("paisInp");
+
+paisInp.addEventListener("paisInp", () => {
+    let texto = paisInp.value;
+    let textoMinuscula = texto.toLowerCase();
+  
+    if (texto !== textoMinuscula) {
+      input.value = textoMinuscula;
+    }
+  });
+
+buscarBtn.addEventListener("click", () => {
+    let paisName = paisInp.value.toLowerCase(); // Convertir a minúsculas
+    let finalURL = `https://restcountries.com/v3.1/name/${paisName}`;
+    console.log(finalURL);
+    fetch(finalURL)
+        .then((response) => response.json())
+        .then((data) => {
+          
+            resultado.innerHTML = `
+            <img src="${data[0].flags.svg}" class="flagImg">
+            <h2> ${data[0].name.common} </h2>
+            <div class="wrapper">
+            <div class="wrapper">
+                <div class="dataWrapper">
+                     <h4>Moneda</h4>
+                     <span>${data[0].currencies[Object.keys(data[0].currencies)].name} - ${Object.keys(data[0].currencies)}</span>
+                </div>
+            </div>
+            `
+            
+        });  
+});
---
+++ endpointPoblacion.js
@@ -0,0 +1,62 @@
+let buscarBtn = document.getElementById("buscarBtn");
+let paisInp = document.getElementById("paisInp");
+
+paisInp.addEventListener("paisInp", () => {
+    let texto = paisInp.value;
+    let textoMinuscula = texto.toLowerCase();
+  
+    if (texto !== textoMinuscula) {
+      input.value = textoMinuscula;
+    }
+  });
+
+buscarBtn.addEventListener("click", () => {
+    let paisCode = paisInp.value.toLowerCase(); // Convertir a minúsculas
+    let finalURL = `https://restcountries.com/v3.1/alpha/${paisCode}`;
+    console.log(finalURL);
+    fetch(finalURL)
+        .then((response) => response.json())
+        .then((data) => {
+          
+            resultado.innerHTML = `
+            <img src="${data[0].flags.svg}" class="flagImg">
+            <h2> ${data[0].name.common} </h2>
+            <div class="wrapper">
+                <div class="dataWrapper">
+                     <h4>Capital</h4>
+                     <span>${data[0].capital[0]}</span>
+                </div>
+            </div>
+            <div class="wrapper">
+                <div class="dataWrapper">
+                     <h4>Continente</h4>
+                     <span>${data[0].continents[0]}</span>
+                </div>
+            </div>
+            </div>
+            <div class="wrapper">
+                <div class="dataWrapper">
+                     <h4>Poblacion</h4>
+                     <span>${data[0].population}</span>
+                </div>
+            </div>
+            <div class="wrapper">
+                <div class="dataWrapper">
+                     <h4>Moneda</h4>
+                     <span>${data[0].currencies[Object.keys(data[0].currencies)].name} - ${Object.keys(data[0].currencies)}</span>
+                </div>
+            </div>
+            <div class="wrapper">
+                <div class="dataWrapper">
+                     <h4>Idioma</h4>
+                     <span>${Object.values(data[0].languages).toString().split(", ").join(", ")}</span>
+                </div>
+            </div>
+
+            `
+            
+        });  
+});
+
+
+
